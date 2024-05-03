b1924d1f2d7f8640e233b708e88e685f1844d29f
Khushi <112639365+khushi075@users.noreply.github.com>
2024-05-03 16:23:17 +0530

    responsive navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,25 +9,25 @@ export const Navbar = () => {
     const [user] = useAuthState(auth)
     const signUserOut = async () => {
         await signOut(auth)
-        
+
     }
     return (
         <div className="flex justify-center text-primary">
             <div className="container mt-4 px-1 py-2 border border-primary rounded-full flex justify-between items-center w-10/12">
-                <div className="ml-3 font-medium text-lg">
+                <div className="lg:ml-3 lg:font-medium lg:text-lg ml-2">
                     <Link to='/'>moussaka</Link>
                 </div>
                 <div className="flex justify-center items-center mr-3 gap-4">
                     {user && (
                         <>
-                            <button className="mr-10 flex items-center justify-center gap-1">
+                            <button className="lg:mr-10 flex items-center justify-center gap-1">
                                 <FontAwesomeIcon icon={faPlus} className="size-5"/>
-                                <Link to='/createpost' className="hover:underline">Create Post</Link>
+                                <Link to='/createpost' className="hover:underline hidden md:block">Create Post</Link>
                             </button>
                             <div className="flex justify-center items-center gap-2">
                                 {/* <FontAwesomeIcon icon={faUserSecret}/> */}
                                 <img src={user?.photoURL || './default.jpeg'} alt="userImg" width="25" height="25" className="rounded-md" />
-                                <div className="">{user?.displayName}</div>
+                                <div className="hidden lg:block">{user?.displayName}</div>
                             </div>
                             <div className="border border-primary rounded-full px-3 py-1 text-white  hover:text-primary bg-primary hover:bg-white">
                                 <button onClick={signUserOut}>Logout</button>
