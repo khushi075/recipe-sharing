e0cf52b17c2aefc56fc85b9f8a1052594a95b593
Khushi <112639365+khushi075@users.noreply.github.com>
2024-05-04 15:03:18 +0530

    ui change

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -37,7 +37,7 @@ export const Main = () => {
 
     return (
         <div className="flex items-center justify-center">
-            {!user && <div className="text-2xl mt-10 font-medium text-primary flex self-center">Login to Fire Up Amazing Recipes and Tips from Around the World</div>}
+            {!user && <div className="text-2xl mt-10 font-medium text-primary flex self-center">Login to Explore Amazing Recipes and Tips from Around the World</div>}
             {user && <div className="flex flex-row flex-wrap items-center justify-center gap-20 mt-10">
                 {postsList?.map((post) => {
                     return <Post post={post} />
